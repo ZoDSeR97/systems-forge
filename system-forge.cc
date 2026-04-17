a20789bcbdf31413d45f042078c8147a86c3347f
ZoDSeR SyLVaN <30656356+ZoDSeR97@users.noreply.github.com>
2026-04-17 01:30:43 -0400

    Initialize CMake project with presets and add main application files

---
+++ system-forge.cc
@@ -0,0 +1,12 @@
+﻿// system-forge.cpp : Defines the entry point for the application.
+//
+
+#include "system-forge.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
